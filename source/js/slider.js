2b37c17e3d6311c4af67f63e14087d3981769c49
RezaGalieva <95533022+RezaGalieva@users.noreply.github.com>
2023-02-16 12:21:10 +0400

    Task-5

---
+++ source/js/slider.js
@@ -0,0 +1,10 @@
+// Слайдер показа до и после
+const indexExample = document.querySelector('.example-wrapper');
+const buttonBefore = indexExample.querySelector('.slider-button--before');
+const buttonAfter = indexExample.querySelector('.slider-button--after');
+const controlSlider = indexExample.querySelector('.control-slider');
+const catFat = indexExample.querySelector('.cat-example--cat-fat');
+const catSlim = indexExample.querySelector('.cat-example--cat-slim');
+const mediaMobile = window.matchMedia('(max-width:768px)');
+
+// Слайдер для мобильной версии
