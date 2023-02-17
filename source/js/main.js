c678d2d75bc1c20159e907db14e2c921bbb78b38
RezaGalieva <95533022+RezaGalieva@users.noreply.github.com>
2023-02-17 14:06:32 +0400

    Task-6

--- source/js/main.js
+++ source/js/main.js
@@ -1,2 +1,36 @@
-import './navigation.js';
-import './slider.js';
+// Открывание и закрывание навигации сайта
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.page-header-toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navToggle.classList.add('page-header-toggle--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navToggle.classList.remove('page-header-toggle--opened');
+  }
+});
+
+// Показ изображения до и после
+const indexExample = document.querySelector('.example-wrapper');
+const buttonBefore = indexExample.querySelector('.slider-button--before');
+const buttonAfter = indexExample.querySelector('.slider-button--after');
+const controlSlider = indexExample.querySelector('.control-slider');
+const catWrapperSlim = indexExample.querySelector('.cat-slim');
+const catWrapperFat = indexExample.querySelector('.cat-fat');
+const catFat = indexExample.querySelector('.cat-example--cat-fat');
+const catSlim = indexExample.querySelector('.cat-example--cat-slim');
+
+buttonBefore.addEventListener('click', () => {
+  catWrapperFat.classList.add('cat-example--cat-fat');
+  catWrapperSlim.classList.remove('cat-example--cat-slim');
+  controlSlider.style.marginLeft = `6px`;
+});
+buttonAfter.addEventListener('click', () => {
+  catWrapperSlim.classList.add('cat-example--cat-slim');
+  catWrapperFat.classList.remove('cat-example--cat-fat');
+  controlSlider.style.marginLeft = `54px`;
+});
